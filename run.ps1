7d8732101e2c082225adf6af5715c39c700e46de
Hou <aplazl55@gmail.com>
2022-10-10 05:10:06 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -7,7 +7,7 @@ Function Set-WallPaper($Value)
  rundll32.exe user32.dll, UpdatePerUserSystemParameters
 
 }
-Invoke-WebRequest -Uri "https://i.ibb.co/8BbpnhM/wallpaper.png" -OutFile "$env:USERPROFILE\Desktop\wallpaper.bmp"
+Invoke-WebRequest -Uri "https://images.squarespace-cdn.com/content/v1/5e269dc38e3cf11eba06a558/1585482069847-M9VWQDJPRBNKW3LRR356/image-asset.jpeg" -OutFile "$env:USERPROFILE\Desktop\wallpaper.bmp"
 Set-WallPaper -value "$env:USERPROFILE\Desktop\wallpaper.bmp"
 
 
