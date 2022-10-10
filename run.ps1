d7410e53b3facdbd456b08402bce8e3283cad8b1
Hou <aplazl55@gmail.com>
2022-10-10 07:21:53 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,7 +1,3 @@
-Function Set-WallPaper($Value)
-{
-Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
-rundll32.exe user32.dll, UpdatePerUserSystemParameters
-}
 Invoke-WebRequest -Uri "https://images.squarespace-cdn.com/content/v1/5e269dc38e3cf11eba06a558/1585482069847-M9VWQDJPRBNKW3LRR356/image-asset.jpeg" -OutFile "$env:USERPROFILE\Desktop\wallpaper.bmp"
-Set-WallPaper -value "$env:USERPROFILE\Desktop\wallpaper.bmp"
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$env:USERPROFILE\Desktop\wallpaper.bmp"
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
